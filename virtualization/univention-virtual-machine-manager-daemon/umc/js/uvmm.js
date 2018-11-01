19b6d932d0007dad65d9c94a6ec2b63d936afc8e
Philipp Hahn <hahn@univention.de>
2018-11-01 11:38:04 +0100

    Bug #47617 uvmm: Cleanup white space
    double spaces
    space after tab
    tab after space
    indention by space
    empty line with only white space

--- virtualization/univention-virtual-machine-manager-daemon/umc/js/uvmm.js
+++ virtualization/univention-virtual-machine-manager-daemon/umc/js/uvmm.js
@@ -790,7 +790,7 @@ define([
 			this.addChild(page);
 			this.selectChild(page);
 		},
-		
+
 		_addDomain: function(values) {
 			var wizard = null;
 
@@ -1265,7 +1265,7 @@ define([
 			}
 
 			// else type == 'domain'
-			var domainColumns =  [{
+			var domainColumns = [{
 				name: 'label',
 				label: _('Name'),
 				formatter: lang.hitch(this, 'iconFormatter')
@@ -1291,8 +1291,8 @@ define([
 				label: _('Description'),
 				formatter: lang.hitch(this, '_descriptionFormatter')
 			};
-			
-		   var cpuusage = {
+
+			var cpuusage = {
 				name: 'cpuUsage',
 				label: _('CPU usage'),
 				style: 'min-width: 80px;',
@@ -1888,6 +1888,6 @@ define([
 
 		onCloseTab: function() {
 			// event stub
- 		}
+		}
 	});
 });
