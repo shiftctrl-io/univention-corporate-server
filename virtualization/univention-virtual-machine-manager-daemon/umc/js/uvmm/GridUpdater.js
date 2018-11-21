15adea0c5a327813b5eb2e5d6e3a636f748365d3
Jürn Brodersen <brodersen@univention.de>
2018-11-21 23:44:01 +0100

    Bug #48083: Fix tree update and show ressources in tree

--- virtualization/univention-virtual-machine-manager-daemon/umc/js/uvmm/GridUpdater.js
+++ virtualization/univention-virtual-machine-manager-daemon/umc/js/uvmm/GridUpdater.js
@@ -31,27 +31,70 @@
 define([
 	"dojo/_base/declare",
 	"dojo/_base/lang",
+	"dojo/_base/array",
+	"dojo/aspect",
 	"dojox/timing/_base",
 	"dijit/Destroyable"
-], function(declare, lang, timing, Destroyable) {
+], function(declare, lang, array, aspect, timing, Destroyable) {
 	return declare("umc.modules.uvmm.GridUpdater", [Destroyable], {
 		grid: null, // reference to the grid
 		tree: null, // reference to the tree
 		interval: null, // interval in seconds
+		_childrenAdvicer: null,
 
 		constructor: function(kwArgs) {
 			lang.mixin(this, kwArgs);
 			this.interval = parseInt(this.interval, 10);
 			var milliseconds = 1000 * this.interval;
+			this._treeStoreCache = {};
+			this._renewChildrenAdvicer();
+			this.own(aspect.after(this.tree, 'reload', lang.hitch(this, '_renewChildrenAdvicer')));
 			this._timer = new timing.Timer(1000 * this.interval);
 			this._timer.onTick = lang.hitch(this, function() {
 				this.grid.update();
+				this._treeUpdate();
 			});
 			if (milliseconds > 0) {
 				// else: interval=0 or interval=NaN
 				this._timer.start();
 			}
 		},
+
+		_renewChildrenAdvicer: function() {
+			if (this._childrenAdvicer) {
+				this._childrenAdvicer.remove();
+			}
+			this._childrenAdvicer = aspect.after(
+				this.tree._gridTree.collection,
+				'getChildren',
+				lang.hitch(this, '_setUpTreeCache')
+			);
+			this.own(this._childrenAdvicer);
+		},
+
+		_treeUpdate: function() {
+			var commands = {default: 'uvmm/node/query', cloudconnections: 'uvmm/cloud/query'};
+			for (var type in this._treeStoreCache) {
+				this.tree.model.getNodes(commands[type], lang.hitch(this, '_updateTreeStore', type));
+			}
+		},
+
+		_updateTreeStore: function(type, nodes) {
+			array.forEach(nodes, function(node) {
+				this._treeStoreCache[type].put(node);
+			}, this);
+		},
+
+		_setUpTreeCache: function(store, parentItems) {
+			var parentItem = parentItems[0];
+			if (parentItem.type !== "group") {
+				return store;
+			}
+			this._treeStoreCache[parentItem.id] = store;
+			return store;
+		},
+
+		_treeStoreCache: null,
 		
 		destroy: function() {
 			this._timer.stop();
