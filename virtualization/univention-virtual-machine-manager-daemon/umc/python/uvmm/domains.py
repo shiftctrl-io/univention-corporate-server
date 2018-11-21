5db154a5d8cb297920c7a494ef4dd331503753f6
Jürn Brodersen <brodersen@univention.de>
2018-11-21 23:42:40 +0100

    Bug #22695 uvmm: Add IO error message to frontend
    mkdir /small
    mount -t tmpfs -o size=1M,mode=755 small /small
    qemu-img create -f qcow2 /small/small.qcow2 1G
    chown libvirt-qemu: /small/small.qcow2
    virsh start "${VM:-ucs43}"
    virsh attach-disk --live --domain "${VM:-ucs43}" --type disk --driver qemu --subdriver qcow2 --cache unsafe --sourcetype file --source /small/small.qcow2 --targetbus virtio --target vdb
    univention-ssh <(exec printf univention) -b -n "${IP:-10.200.17.22}" 'dd if=/dev/zero of=/dev/vdb bs=4096 count=1024'
    uvmm domain_info "qemu://$(hostname -f)/system" "$(virsh domuuid "${VM:-ucs43}")" | grep -A3 "'error'"

--- virtualization/univention-virtual-machine-manager-daemon/umc/python/uvmm/domains.py
+++ virtualization/univention-virtual-machine-manager-daemon/umc/python/uvmm/domains.py
@@ -125,6 +125,7 @@ class Domains(object):
 						'suspended': bool(domain['suspended']),
 						'description': domain['description'],
 						'node_available': domain['node_available'],
+						'error': domain['error'],
 					})
 			return domain_list
 
