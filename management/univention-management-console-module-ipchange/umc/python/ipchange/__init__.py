a649766eb0da7aa1d4e079c1ef10947c7f096404
Philipp Hahn <hahn@univention.de>
2019-02-19 10:07:39 +0100

    Bug #48698 spell: addres -> address

--- management/univention-management-console-module-ipchange/umc/python/ipchange/__init__.py
+++ management/univention-management-console-module-ipchange/umc/python/ipchange/__init__.py
@@ -58,10 +58,10 @@ class Instance(Base):
 		'''Return a dict with all necessary values for ipchange read from the current
 		status of the system.'''
 
-		# ignore link local addresses (no DHCP adress received)
+		# ignore link local addresses (no DHCP address received)
 		network = ipaddr.IPv4Network('%s/%s' % (ip, netmask))
 		if network.IsLinkLocal():
-			MODULE.error('Ignore link local adress change.')
+			MODULE.error('Ignore link local address change.')
 			return
 
 		lo, position = univention.admin.uldap.getAdminConnection()
