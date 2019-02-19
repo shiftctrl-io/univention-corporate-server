a1da2719f88776e152b9b6459e70ba961c0ab59f
Philipp Hahn <hahn@univention.de>
2019-02-19 10:08:32 +0100

    Bug #48698 spell: Similiar -> Similar

--- management/univention-web/js/widgets/MultiInput.js
+++ management/univention-web/js/widgets/MultiInput.js
@@ -773,7 +773,7 @@ define([
 		},
 
 		// ready:
-		//		Similiar to `umc/widgets/_FormWidgetMixin:ready`.
+		//		Similar to `umc/widgets/_FormWidgetMixin:ready`.
 		ready: function() {
 			return this._readyDeferred;
 		}
