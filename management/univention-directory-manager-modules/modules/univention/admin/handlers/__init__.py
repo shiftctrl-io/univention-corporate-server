c0f9a1e114a3dd3521bd34d5c1d16f1042f7813c
Philipp Hahn <hahn@univention.de>
2019-02-19 10:07:58 +0100

    Bug #48698 spell: excetion -> exception

--- management/univention-directory-manager-modules/modules/univention/admin/handlers/__init__.py
+++ management/univention-directory-manager-modules/modules/univention/admin/handlers/__init__.py
@@ -2476,7 +2476,7 @@ class simpleComputer(simpleLdap):
 				zone.open()
 				zone.modify()
 			else:
-				# thow exeption, cNAMERecord is single value
+				# thow exception, cNAMERecord is single value
 				raise univention.admin.uexceptions.dnsAliasAlreadyUsed, _('DNS alias is already in use.')
 
 	def __remove_dns_alias_object(self, name, dnsForwardZone, dnsAliasZoneContainer, alias=None):
@@ -2503,7 +2503,7 @@ class simpleComputer(simpleLdap):
 							zone.open()
 							zone.modify()
 					else:
-						# could thow some exeption
+						# could thow some exception
 						pass
 			else:
 				if dnsForwardZone:
