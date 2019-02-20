3978ca0c3ece8d7a177fe93e4c6060e3ab2d4527
Philipp Hahn <hahn@univention.de>
2019-02-20 09:01:56 +0100

    Revert "Bug #48698 spell: realy -> really"
    This reverts commit c00b5ade433a9ab986a08e9957cecd5c069d80b6.
    
    This is "real-x" and "real-y".

--- virtualization/univention-novnc/include/input.js
+++ virtualization/univention-novnc/include/input.js
@@ -645,9 +645,9 @@ function onMouseDisable(e) {
     evt = (e ? e : window.event);
     pos = Util.getEventPosition(e, conf.target, conf.scale);
     /* Stop propagation if inside canvas area */
-    if ((pos.realx >= 0) && (pos.really >= 0) &&
+    if ((pos.realx >= 0) && (pos.realy >= 0) &&
         (pos.realx < conf.target.offsetWidth) &&
-        (pos.really < conf.target.offsetHeight)) {
+        (pos.realy < conf.target.offsetHeight)) {
         //Util.Debug("mouse event disabled");
         Util.stopEvent(e);
         return false;
